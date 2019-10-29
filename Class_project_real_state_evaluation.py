b9eb379a60736cd6945c3adce2a6bfa4aed7674f
Manjeet Maan <49412434+MannSaab@users.noreply.github.com>
2019-10-29 15:23:25 +0530

    Update Class_project_real_state_evaluation.py

--- Class_project_real_state_evaluation.py
+++ Class_project_real_state_evaluation.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 
-#reading data file 
+                                    #reading data file 
 df=pd.read_excel("C:/Users/Abhi/Desktop/problem/real_state.xlsx")
 print(df.head())
 df=df.rename(columns={"X1 transaction date":"X1","X2 house age":"X2","X3 distance to the nearest MRT station":"X3","X4 number of convenience stores":"X4","X5 latitude":"X5","X6 longitude":"X6","Y house price of unit area":"Y"})
@@ -16,7 +16,7 @@ print(df.count())
 print(df.dtypes)
 print(df.describe())
 
-#remove useless column "NO" 
+                                    #remove useless column "NO" 
 df=df.drop("No",axis=1)
 print(df.head())
 #seperating the labes from the dataset
@@ -33,6 +33,6 @@ scaler=StandardScaler()
 scaler.fit(x_train)
 x_train_scaled=scaler.transform(x_train)
 x_test_scaled=scaler.transform(x_test)
-
+print(x_test_scaled)
 
 
